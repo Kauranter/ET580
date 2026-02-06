48d4fdd8fb4745dd5f5ea94c9c46906d1d9d4f14
Kauranter <anterpreet.kaur55@student.qcc.cuny.edu>
2026-02-06 00:54:11 +0000

    TASK A done

---
+++ GIT/GIT.cpp
@@ -0,0 +1,13 @@
+/*
+Name: Anterpreet Kaur
+Class: ET 580
+Homework: 2 (GIT BASICS)
+Date: Feb 5th, 2026
+*/
+
+#include <iostream>
+int main ()
+{
+    std::cout<< "Anterpreet Task A\n";
+    return 0;
+}
